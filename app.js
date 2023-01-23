7dfe14b02887b8deae6dcb47a53c275015bab977
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 11:44:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ require('./db/conn'); // requiring database connection info
 //   }),
 // );
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Credentials", "true");
+     res.header("Access-Control-Allow-Credentials", "true");
      res.header("Access-Control-Allow-Origin", "https://hptech-v16r.onrender.com");
+     res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+     res.header("Access-Control-Allow-Headers: Content-Type, *");
 
    next();
 })
