609c42baf39eedcfdaa9292f4b786d947c5ae63e
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 08:02:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require("cors")
 const PORT = process.env.PORT ||5000; // accessing  credentials from env file
 
 require('./db/conn'); // requiring database connection info
-app.use(cors({origin: "*"}))
+app.use(cors({origin: "https://hptech-v16r.onrender.com"}))
 
 const auth = require ('./router/auth');
 app.use(auth);
