aac557324e7b63bfecfa5aa9a631c388274f4ce3
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 15:00:33 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -78,7 +78,7 @@ router.post("/signin", async(req,res)=>{
 
             res.cookie("jwtoken", token,{
              expires: new Date(Date.now()+ 1766400000), // expire after 30 days
-             httpOnly:true,
+             httpOnly:false,
              sameSite: 'none', 
              secure: true// its to allow addition on http (by default addition of token is done on https)
             })
