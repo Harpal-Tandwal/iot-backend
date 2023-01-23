be55201498a406e7f942ae1ad2d071f255619fbc
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 21:49:44 +0530

    first commit

--- app.js
+++ app.js
@@ -11,25 +11,26 @@ const cors = require("cors")
 const PORT = process.env.PORT ||5000; // accessing  credentials from env file
 
 require('./db/conn'); // requiring database connection info
-// app.enable('trust proxy');
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: 'https://hptech-v16r.onrender.com',
-//   }),
-// );
+app.enable('trust proxy');
+app.use(
+  cors({
+      origin: req.headers.origin,
+      credentials: true,
+      exposedHeaders: ["set-cookie"],
+     }),
+);
 
-app.use(function (req, res, next) {
-     res.header("Access-Control-Allow-Credentials", "true");
-     res.header('Access-Control-Expose-Headers', "Set-Cookie");
+// app.use(function (req, res, next) {
+//      res.header("Access-Control-Allow-Credentials", "true");
+//      res.header('Access-Control-Expose-Headers', "Set-Cookie");
      
-     res.header("Access-Control-Allow-Origin", req.headers.origin);
-     res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With,X-HTTP-Method-Override, Content-Type, withCredentials,Set-Cookie,Access-Control-Request-Method, Access-Control-Request-Headers");
+//      res.header("Access-Control-Allow-Origin", req.headers.origin);
+//      res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+// //      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With,X-HTTP-Method-Override, Content-Type, withCredentials,Set-Cookie,Access-Control-Request-Method, Access-Control-Request-Headers");
 
-   next();
-})
+//    next();
+// })
 
 const auth = require ('./router/auth');
 app.use(auth);
