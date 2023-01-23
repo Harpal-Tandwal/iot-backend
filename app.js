5242e8e0e56470fc8cfa424a8aa37d0cf3d3407c
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 08:52:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const cors = require("cors")
 const PORT = process.env.PORT ||5000; // accessing  credentials from env file
 
 require('./db/conn'); // requiring database connection info
-app.use(cors({origin: "https://hptech-v16r.onrender.com"}))
+app.use(
+  cors({
+    credentials: true,
+    origin: 'https://hptech-v16r.onrender.com',
+  }),
+);
 app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
