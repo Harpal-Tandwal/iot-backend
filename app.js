501f5dfdd84d246ea61bfc99934b38d0f74a0719
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 11:53:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
      res.header("Access-Control-Allow-Credentials", "true");
      res.header("Access-Control-Allow-Origin", "https://hptech-v16r.onrender.com");
      res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-     res.header("Access-Control-Allow-Headers: Content-Type, *");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
    next();
 })
