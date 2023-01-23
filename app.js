ecfd0f17711b5ad44486c0ec3f79dc7760888622
Harpal-Tandwal <92112465+Harpal-Tandwal@users.noreply.github.com>
2023-01-23 08:57:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use(
     origin: 'https://hptech-v16r.onrender.com',
   }),
 );
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+// app.use(function (req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+// })
 
 const auth = require ('./router/auth');
 app.use(auth);
